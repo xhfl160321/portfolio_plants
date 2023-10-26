abb9ff6a4ee1382723311981d2dd6b70efc80f8d
sueng0422 <sueng0422@naver.com>
2023-10-27 00:24:31 +0900

    반응형 미디어  쿼리 추가

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
 (function(){
+  // 메뉴 호버 이벤트
     $(".menu > ul > li").hover(function(){
       $(this).find(".subMenu").stop().slideDown(500);
     }, function(){
@@ -12,7 +13,7 @@
       $(this).find(".p2imgCon").stop().slideToggle(500, 'linear');
     });
     
-  //   email check
+  //  이메일 유효성 검사 스크립트(구글 참조)
     function email_check( email ) {    
       var regex=/([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
       return (email != '' && email != 'undefined' && regex.test(email)); 
@@ -26,7 +27,8 @@
         $(this).focus();
         return false;
       }else {
-      $(".result-email").text('');
+        document.getElementById("p4Form").submit();
+        alert('Thank you for subscribe!');
       }
     });
   
@@ -36,7 +38,12 @@
       };
     }, true);
 
-    //클릭 시 부드러운 scroll down
+    // 이메일에 아무 입력 값이 없을 때 데이터 전송을 막기위한 함수
+    $("#p4Form").submit(function(e){
+      e.preventDefault();
+    });
+
+    //클릭 시 부드러운 scroll down(구글 참조)
     $('a[href*="#"]').on('click', function (e) {
       e.preventDefault();
   
